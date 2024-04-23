25c48f728e440d8b75a817e46363f980fec9690e
wlricardo <wlricardo@gmail.com>
2024-04-22 22:57:29 -0300

    Melhorando a exibição do programa no console

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using DesafioPOO.Models;
 
-Console.WriteLine("Smartphone Nokia");
+Console.WriteLine("\nSmartphone Nokia");
 Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111", memoria: 64);
 nokia.Ligar();
 nokia.InstalarAplicativo("WhatsApp");
@@ -10,4 +10,4 @@ Console.WriteLine("\n");
 Console.WriteLine("Smartphone iPhone");
 Smartphone iphone = new Iphone(numero: "654321", modelo: "Modelo 2", imei: "22222222", memoria: 128);
 iphone.Ligar();
-iphone.InstalarAplicativo("Telegram");
+iphone.InstalarAplicativo("Telegram\n");
